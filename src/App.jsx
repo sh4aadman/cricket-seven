db680a4db59bdc6215d3e63ea825dfe9c270066f
sh4aadman <sh4adman@gmail.com>
2026-06-30 15:05:08 +0600

    navbar created

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./components/ui/navbar"
+
+function App() {
+
+  return (
+    <>
+      <Navbar/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/ui/navbar.jsx
@@ -0,0 +1,24 @@
+import logo from "../../assets/logo.png"
+
+function Navbar() {
+  return (
+    <nav className="flex justify-between">
+      <img className="w-[5%]" src={logo} alt="logo" />
+      <section className="flex items-center gap-14">
+        <ul className="flex gap-14 text-[#131313] text-lg opacity-70">
+          <li><a href="#Home">Home</a></li>
+          <li><a href="#Fixturess">Fixture</a></li>
+          <li><a href="#Teams">Teams</a></li>
+          <li><a href="#Schedules">Schedules</a></li>
+        </ul>
+        <section className="font-medium text-[#131313] border border-[#1313131A] px-5 py-4 rounded-xl">
+          <span className="mr-1 text-lg">0</span>
+          <span className="text-lg">Coin</span>
+          <i class="fa-solid fa-dollar-sign ml-2 p-1 border-2 rounded-full"></i>
+        </section>
+      </section>
+    </nav>
+  );
+}
+
+export default Navbar;
